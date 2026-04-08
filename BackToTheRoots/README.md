ddc6a00156ae59436937238f535682556db04f18
Zach Gillis <45103920+zachgillis@users.noreply.github.com>
2026-04-08 15:24:31 -0700

    Add files via upload

---
+++ BackToTheRoots/README.md
@@ -0,0 +1,95 @@
+# How to produce paper results
+
+## Data
+
+The dataset needed to produce these results can be found in https://github.com/uhh-pd-ml/treebased_anomaly_detection. 
+
+## NN runs:
+
+For all NN runs, the ensembling is performed externally, producing ROC curve files for an ensemble of 10 and 50 NNs. For this do: 
+
+```
+    python do_ensemble_NN.py --directory "results/NN_to_be_ensembled/"
+```
+
+Note: for 1D scan loop is possible in program using option --scan_1D
+
+## Different runs for paper:
+
+### Baseline performances:
+
+```
+    python run_pipeline.py --mode "supervised" --classifier "NN" --input_set "baseline" --directory "results/supervised_baseline_NN/"
+    python run_pipeline.py --mode "supervised" --classifier "BDT" --input_set "baseline" --directory "results/supervised_baseline_BDT/"
+    python run_pipeline.py --mode "IAD" --classifier "NN" --input_set "baseline" --directory "results/IAD_baseline_NN/"
+    python run_pipeline.py --mode "IAD" --classifier "BDT" --input_set "baseline" --directory "results/IAD_baseline_BDT/"
+```
+
+### Gaussian inputs for classifier type X and N gaussian inputs:
+
+Performed for:
+- X in ["NN", "BDT"]
+- N in [1, 2, 5, 10, 30, 50]
+
+```
+    python run_pipeline.py --mode "IAD" --classifier X --input_set "baseline" --gaussian_inputs N --directory "results/IAD_X_NG/" 
+```
+
+### All other sets for classifier X and input_set A:
+
+Performed for:
+- X in ["BDT", "NN"]
+- A in ["extended1", "extended2", "extended3"] (Note: "baseline" already performed for baseline performance comparisons)
+
+```
+    python run_pipeline.py --mode "IAD" --classifier X --input_set A --directory "results/IAD_X_A/" 
+```
+
+### 1D scan for classifier X and input_set A:
+
+Performed for:
+- X = "BDT" and A in ["baseline", "extended3"]
+
+```
+    sig = (0 100 200 300 400 500 750 1000 1200 1500 2000)
+    for s in ${sig}; do
+        python run_pipeline.py --mode "IAD" --classifier X  --input_set A  --randomize_seed --signal_number ${s} --directory "results/1D_Nsig_X_A/Nsig_${s}/"
+    done
+```
+
+Performed for: 
+- X = "NN" and A = "baseline"
+```
+    sig = (0 100 200 300 400 500 750 1000 1200 1500 2000)
+    for ((i=1; i<12; i++)); do
+        python run_pipeline.py --mode "IAD" --classifier X  --input_set A  --set_seed ${i} --signal_number ${sig[i]} --directory "results/1D_Nsig_X_A/Nsig_${s}/"
+    done
+```
+
+
+### 2D scan
+
+Performed for:
+- X = "BDT"
+- A in ["baseline", "extended3"]
+
+```
+    bkg=(25000 50000 75000 100000 125000 150000 175000 200000)
+    sig=(0.25 0.5 0.75 1.0 1.25 1.5 1.75 2.0 2.25 2.5)
+
+    for b in ${bkg}; do
+        for s in ${sig} do
+            python run_pipeline.py --mode "IAD" --classifier X --input_set A --randomize_seed --scan_2D --signal_significance ${s} --N_bkg ${b} --directory "results/2D_X_A/${b}_${b}_${s}/"
+        done
+    done
+```
+
+### Three pronged signal for classifier X and input_set A:
+
+Performed for:
+- X in ["BDT", "NN"]
+- A in ["baseline", "extended1", "extended2", "extended3"]
+
+```
+    python run_pipeline.py --mode "IAD" --classifier X --input_set A --three_pronged --directory "results/IAD_X_A/" 
+```
---
+++ BackToTheRoots/run_pipeline.py
@@ -0,0 +1,126 @@
+import numpy as np
+import dataprep_utils as dp
+import plotting_utils as pf
+import argparse
+import os
+
+parser = argparse.ArgumentParser(
+    description='Run the full analysis chain.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+#For SIC curve reproduction only these 5 option need to be changed
+parser.add_argument('--mode', type=str, choices=["IAD", "supervised"], required=True)
+parser.add_argument('--directory', type=str, required=True)
+parser.add_argument('--classifier', type=str, choices=["BDT", "NN"], required=True)
+parser.add_argument('--input_set', type=str, choices=["baseline","extended1","extended2","extended3","kitchensink", "kitchensink_super"])
+parser.add_argument('--gaussian_inputs', type=int, default=None)
+
+#Data files
+parser.add_argument('--data_file', type=str, default="/hpcwork/rwth0934/LHCO_dataset/extratau2/events_anomalydetection_v2.extratau_2.features.h5")
+parser.add_argument('--extrabkg_file', type=str, default="/hpcwork/rwth0934/LHCO_dataset/extratau2/events_anomalydetection_DelphesPythia8_v2_qcd_extra_inneronly_combined_extratau_2_features.h5")
+parser.add_argument('--signal_file', type=str, default=None)
+parser.add_argument('--three_pronged', default=False, action="store_true")
+
+#Dataset preparation
+parser.add_argument('--signal_percentage', type=float, default=None, help="Third in priority order")
+parser.add_argument('--signal_number', type=int, default=None, help="Second in priority order")
+parser.add_argument('--minmass', type=float, default=3.3)
+parser.add_argument('--maxmass', type=float, default=3.7)
+parser.add_argument('--cl_logit', default=False, action="store_true")
+parser.add_argument('--cl_norm', default=True, action="store_false")
+parser.add_argument('--N_normal_inputs', default=4, type=int, help="Needed only for gaussian inputs")
+parser.add_argument('--supervised_normal_signal', default=False, action="store_true")
+parser.add_argument('--set_seed', type=int, default=1)
+parser.add_argument('--randomize_seed', default=False, action="store_true")
+parser.add_argument('--inputs', type=int, default=4)
+
+#2D scan
+parser.add_argument('--scan_2D', default=False, action="store_true")
+parser.add_argument('--N_CR', type=int, default=None)
+parser.add_argument('--N_bkg', type=int, default=None)
+parser.add_argument('--signal_significance', type=float, default=None, help="Takes highest priority, only supported for 2D_scan")
+
+#General classifier Arguments
+parser.add_argument('--N_runs', type=int, default=10)
+parser.add_argument('--ensemble_over', default=50, type=int)
+parser.add_argument('--start_at_run', type=int, default=0)
+parser.add_argument('--cl_N_best_epochs', type=int, default=1)
+parser.add_argument('--cl_filename', type=str, default="classifier4.yml")
+
+args = parser.parse_args()
+
+if not os.path.exists(args.directory):
+	os.makedirs(args.directory)
+
+if args.classifier=="NN":
+    args.N_runs=100
+    args.cl_N_best_epochs=1
+    import NN_utils as NN
+else:
+    import BDT_utils as BDT
+
+if args.input_set=="extended1":
+    args.inputs=10
+elif args.input_set=="extended2":
+    args.inputs=12
+elif args.input_set=="extended3":
+    args.inputs=56
+elif args.input_set=="kitchensink":
+    args.inputs=72
+elif args.input_set=="kitchensink_super":
+    args.inputs=104
+
+if args.three_pronged:
+	args.signal_file = "/hpcwork/rwth0934/LHCO_dataset/extratau2/events_anomalydetection_Z_XY_qqq.extratau_2.features.h5"
+
+if args.gaussian_inputs is not None:
+    args.inputs+=args.gaussian_inputs
+
+if args.scan_2D: 
+    if args.N_CR is None:
+        args.N_CR = args.N_bkg
+    if not args.signal_significance:
+        raise ValueError("need signal significance for 2D_scan")
+
+if args.signal_significance is not None:
+    if not args.scan_2D: 
+        raise ValueError("signal significance only supported for 2D_scan")
+    
+print(args)
+
+if not args.scan_2D:
+    if not args.randomize_seed:
+        X_train, Y_train, X_test, Y_test = dp.classifier_data_prep(args)
+    for i in range(args.start_at_run, args.N_runs):
+        print()
+        print("------------------------------------------------------")
+        print()
+        print("Classifier run no. ", i)
+        print()
+        args.set_seed = i
+        if args.classifier=="NN": 
+            direc_run = args.directory+"run"+str(i)+"/"
+            if args.randomize_seed and i%args.ensemble_over==0:
+                X_train, Y_train, X_test, Y_test = dp.classifier_data_prep(args)
+            NN.classifier_training(X_train, Y_train, X_test, Y_test, args, i, direc_run=direc_run)
+        else:
+            if args.randomize_seed:
+                X_train, Y_train, X_test, Y_test = dp.classifier_data_prep(args)
+            BDT.classifier_training(X_train, Y_train, X_test, Y_test, args, i)
+
+else: 
+    if args.N_bkg is None:
+        raise ValueError("Need to specify N_bkg")
+    if args.classifier=="NN":
+        raise ValueError("NN 2D scan currently not supported.")
+    else:
+        for i in range(args.start_at_run, args.N_runs):
+            print()
+            print("------------------------------------------------------")
+            print()
+            print("Classifier run no. ", i)
+            print()
+            args.set_seed = i
+            if args.randomize_seed:
+                X_train, Y_train, X_test, Y_test = dp.data_prep_2D(args)
+            BDT.classifier_training(X_train, Y_train, X_test, Y_test, args, i)
